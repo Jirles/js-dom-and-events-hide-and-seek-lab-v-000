ec3501633420343dc1cb040ee6b4be3b4ec847ea
Emily Jirles <emily.jirles@gmail.com>
2018-07-14 14:25:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
+
+function nestedTarget(){
+  //pulls .target out of #nested
+  //must be arbitrary with regard to tags
+  return document.querySelector("#nested .target");
+};
+
+function increaseRankBy(n){
+  //increases the ransk of all .ranked-list by n
+  //user parseInt()
+};
+
+function deepestChild(){
+  //pulls out most deeply nested child from div#grand-node
+  //this one doesn't need to be generic
+};
