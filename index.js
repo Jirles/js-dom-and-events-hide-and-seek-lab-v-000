e70d56290c4870b85de5cf63f9a6fd5fcd088119
Emily Jirles <emily.jirles@gmail.com>
2018-07-14 15:22:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
+
+function nestedTarget(){
+  //pulls .target out of #nested
+  //must be arbitrary with regard to tags
+  return document.querySelector("#nested .target");
+};
+
+function increaseRankBy(n){
+  //increases the ranks of all .ranked-list by n
+  //user parseInt()
+  const listItems = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i < listItems.length; i++){
+    listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n
+  }
+};
+
+function deepestChild(){
+  //pulls out most deeply nested child from div#grand-node
+  //this one doesn't need to be generic
+  let node = document.querySelector('#grand-node');
+  while (node.childElementCount > 0) {
+    node = node.children[0];
+  }
+  return node;
+};
